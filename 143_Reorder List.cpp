2975814f3ba5f8c69dc852429b95d5f71884597a
Neo Liu <zhixinl@uoregon.edu>
2017-06-22 09:36:12 +0800

    143_edited

--- 143_Reorder List.cpp
+++ 143_Reorder List.cpp
@@ -24,39 +24,36 @@ public:
 		for (int i = 0; i < split_index; ++ i)
 			second_part = second_part->next;
 
-		ListNode *swap = second_part->next;
+		ListNode          *swap = second_part->next;
 		second_part->next = NULL;
-		second_part = swap;
+		second_part       = swap;
 
 		ListNode *reverse_head = NULL;
 		while (second_part) {
-			ListNode *tem = second_part;
-			second_part = second_part->next;
-			tem->next = reverse_head;
+			ListNode     *tem = second_part;
+			second_part  = second_part->next;
+			tem->next    = reverse_head;
 			reverse_head = tem;
 		}
-		while (head) { cout << head->val; head = head->next; }
-		cout << endl;
-		while (reverse_head) { cout << reverse_head->val; reverse_head = reverse_head->next; }
-		/*ListNode *first_head = head;
-		ListNode *iter = head;
-		
-		while (reverse_head && first_head) {
 
-			first_head = first_head->next;
+		ListNode *first_head = head->next;
+		ListNode *iter = head;
+
+		while (reverse_head && first_head) {
+			
 			iter->next = reverse_head;
 			iter = iter->next;
-
 			reverse_head = reverse_head->next;
+
 			iter->next = first_head;
 			iter = iter->next;
-	
+			first_head = first_head->next;	
 		}
 
 		if (first_head) 
 			iter->next = first_head;
 		if (reverse_head)
 			iter->next = reverse_head;
-	*/
+	
     }
 };
