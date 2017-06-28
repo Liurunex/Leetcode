a15a84a65cc47d8c1b9915c263c63d3b52e06bcc
Zhixin Liu <zxliu@zxliu-mac.freewheelmedia.net>
2017-06-28 10:32:01 +0800

    edited 215

--- 215_Kth Largest Element in an Array.cpp
+++ 215_Kth Largest Element in an Array.cpp
@@ -1,28 +1,45 @@
+/*
+quick sort is the key: 
+http://www.geeksforgeeks.org/quick-sort/
+http://www.cs.yale.edu/homes/aspnes/pinewiki/QuickSelect.html
+http://blog.csdn.net/morewindows/article/details/6684558
+ */
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
- 		int theOne = nums[0];      
-    }
-    int quickSort(vector<int>& nums) {
-    	int i = 1, j = nums.size()-1, theOne = nums[0], theindex = 0;
-    	while (i < j) {
-    		while (1) {
-    			if (nums[j] < theOne) {
-    				nums[theindex] = nums[j];
-    				theindex = j;
-    				break;
+		int left = 0, right = nums.size()-1;
+		int index = quickSort(nums, left, right);
+		
+		while (index + k != nums.size()) {
+			if (index + k > nums.size())
+				right = index - 1;
+			else left = index + 1;
+			
+			index = quickSort(nums, left, right);
+		}
+		return nums[index];
+	}
+    int quickSort(vector<int>& nums, int left, int right) {
+    	int i = left, j = right, theOne = nums[left];
+		while (i < j) {
+    		while (i < j) {
+				if (nums[j] < theOne) {
+    				nums[i] = nums[j];
+    				i ++;
+					break;
     			}
-    			j --; 
+				j --;
     		}
-    		while (1) {
+    		while (i < j) {
     			if (nums[i] > theOne) {
-    				nums[theindex] == nums[i];
-    				theindex = i;
-    				break;
+    				nums[j] =  nums[i];
+    				j --;
+					break;
     			}
     			i ++;
     		}
     	}
-    		
+    	nums[i] = theOne;
+		return i;
     }
 };
