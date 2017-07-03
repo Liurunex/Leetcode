fd1650d2dbc88ef30549ab36b3a418159c128a26
Liurunex <liurunex@gmail.com>
2017-07-03 23:43:17 +0800

    635 edited

--- 635_Design Log Storage System.cpp
+++ 635_Design Log Storage System.cpp
@@ -1,3 +1,4 @@
+/* convert the string to 1 12-digit number, store it in map*/
 class LogSystem {
 public:
 	map<long, int> storeMap;
@@ -17,14 +18,17 @@ public:
 			timeStr += timeSeg;
 		}
 		long timeInt = stol(timeStr);
-		cout << "insert: " << timeInt << endl;
+		
 		if (storeMap.find(timeInt) != storeMap.end())
 			cout << "collision" << endl;
-		else storeMap[timeInt] = id;
+		else {
+			storeMap[timeInt] = id;
+			cout << "insert: " << timeInt << " -> " << storeMap[timeInt] << endl;
+		}
 	}
 	
 	vector<int> retrieve(string s, string e, string gra) {
-		vector<int> res;
+		map<long, int>::iterator i;
 		string ssSeg, eeSeg;
 		string sstimeStr, eetimeStr;
 		int isyear = 1;
@@ -41,28 +45,34 @@ public:
 		}
         long ss = stol(sstimeStr);
         long ee = stol(eetimeStr);
-        string scurtail = "0", ecurtail = "0";
+        string scurtail = "0", ecurtail = "0", esupply = "0";
+        string supply = "999999999999";
         int curtail_pos = 0;
         
-        if(gra == "Year") 			curtail_pos = 4;
-        else if (gra == "Month") 	curtail_pos = 6;
-		else if (gra == "Day")		curtail_pos = 8; 
-		else if (gra == "Hour") 	curtail_pos = 10; 
-		else if (gra == "Minute")	curtail_pos = 12; 
+        if(gra == "Year") 			curtail_pos = 2;
+        else if (gra == "Month") 	curtail_pos = 4;
+		else if (gra == "Day")		curtail_pos = 6; 
+		else if (gra == "Hour") 	curtail_pos = 8; 
+		else if (gra == "Minute")	curtail_pos = 10; 
      
         if (curtail_pos) {
         	scurtail = sstimeStr.substr(curtail_pos); 
 			ecurtail = eetimeStr.substr(curtail_pos);
+            esupply  = supply.substr(curtail_pos);
 		}
 		long sss = ss - stol(scurtail);
-		long eee = ee - stol(ecurtail);
+		long eee = ee - stol(ecurtail) + stol(esupply);
 		cout << sss << " to " <<  eee << endl;
 
+		vector<int> res;
 		map<long, int>::iterator itlow, itup;
 		itlow = storeMap.lower_bound(sss);
 		itup  = storeMap.upper_bound(eee);
-		itup --;
-		for (auto i = itlow; i != itup; ++ i) 
+		//itup --;
+		cout << "lower: " << itlow->second << endl;
+		//cout << "upper: " << itup->second << endl;
+		
+		for (i = itlow; i != itup; ++ i) 
 			res.push_back(i->second);
 		return res;
 	}
@@ -74,3 +84,4 @@ public:
  * obj.put(id,timestamp);
  * vector<int> param_2 = obj.retrieve(s,e,gra);
  */
+
