b670eeeb530559fefb16633fb4ccb8091d6d71ad
Liurunex <liurunex@gmail.com>
2017-07-17 23:26:13 +0800

    125 edited

--- 125_Valid Palindrome.cpp
+++ 125_Valid Palindrome.cpp
@@ -6,15 +6,17 @@ public:
 			while (left < right && !(s[left]-'0' >= 0  && '9'-s[left] >= 0)   
 				&& !(tolower(s[left])-'a' >= 0 && 'z'-tolower(s[left]) >= 0))
 				left ++;
-			while (right > left && !(s[right]-'0' >= 0 && '9'-s[right]) >= 0) 
-				&& !(tolower(s[right]) - 'a' >= 0 || 'z'-tolower(s[right]) >= 0))
+			while (right > left && !(s[right]-'0' >= 0 && '9'-s[right] >= 0) 
+				&& !(tolower(s[right])-'a' >= 0 && 'z'-tolower(s[right]) >= 0))
 				right --;
 			
-			if ((!(s[left]-'0' >= 0  && '9'-s[left] >= 0) && !(tolower(s[left]) - 'a' >= 0 && 'z'-tolower(s[left]) >= 0)) || 
-				((!(s[right]-'0' >= 0 && '9'-s[right]) >= 0) && !(tolower(s[right]) - 'a' >= 0 && 'z'-tolower(s[left]) >= 0)))
-				return true;
+			if ((!(s[left]-'0' >= 0  && '9'-s[left] >= 0) && !(tolower(s[left])-'a' >= 0 && 'z'-tolower(s[left]) >= 0)) || 
+				(!(s[right]-'0' >= 0 && '9'-s[right] >= 0) && !(tolower(s[right])-'a' >= 0 && 'z'-tolower(s[right]) >= 0))) {
+				cout << "out";
+                return true;
+            }
 
-			if (tolower(s[left]) == tolower(s[right])){
+			if (s[left] == s[right] || tolower(s[left]) == tolower(s[right])) {
 				left  ++;
 				right --;
 				continue;
