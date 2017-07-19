effc7da312149178bec694643a716d663543e535
Liurunex <liurunex@gmail.com>
2017-07-19 22:52:43 +0800

    513 edited

--- 513_Find Bottom Left Tree Value.cpp
+++ 513_Find Bottom Left Tree Value.cpp
@@ -11,20 +11,26 @@ class Solution {
 public:
 	int findBottomLeftValue(TreeNode* root) {
 		if (!root) return 0;
-		int leftmost = root->val;
 		queue<pair<TreeNode*, int>> dfsqueue;
 		dfsqueue.push(make_pair(root, 0));
+		pair<TreeNode*, int> levelnode;
+		levelnode.first = root;
+		levelnode.second = 0;
 		while (!dfsqueue.empty()) {
 			pair<TreeNode*, int> node = dfsqueue.front();
 			dfsqueue.pop();
+			
+			if (levelnode.second < node.second) {
+				levelnode.first = node.first;
+				levelnode.second = node.second;
+			}
 
 			if (node.first->left) {
-				leftmost = node.first->left->val;
 				dfsqueue.push(make_pair(node.first->left, node.second+1));
 			}
-			if (node.first->right) 
+			if (node.first->right)
 				dfsqueue.push(make_pair(node.first->right, node.second+1));
 		}
-		return leftmost;
+		return levelnode.first->val;
 	}
 };
