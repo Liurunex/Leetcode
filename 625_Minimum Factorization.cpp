99e8d69e129a02b40d9a9275fd4eb7e84bf59494
zxliu <zxliu@freewheel.tv>
2017-08-03 14:32:42 +0800

    625 added

---
+++ 625_Minimum Factorization.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+	string minium;
+public:
+	int smallestFactorization(int a) {
+		if (a == 1) return a;
+		helper(a, 9);
+		if (a != 1) return 0;
+		return stoll(minium) > INT_MAX ? 0:(int)(stoll(minium));
+	}
+	
+	void helper(int &a, int i) {
+		if (a == 1 || i == 1)
+			return;
+		while (!(a%i)) {
+			minium = to_string(i) + minium; 
+			a /= i;
+		}
+		helper(a, i-1);
+	}	
+};
