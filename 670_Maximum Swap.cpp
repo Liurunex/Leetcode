9e490e0d225e94caf8043470f81ad79d2926bc1f
Liurunex <liurunex@gmail.com>
2017-09-03 11:46:52 -0400

    670 O(n) update

--- 670_Maximum Swap.cpp
+++ 670_Maximum Swap.cpp
@@ -22,3 +22,50 @@ public:
 		return candiate;
 	}
 };
+
+/* O(n) solution: */
+class Solution {
+public:
+	int maximumSwap(int num) {
+		if (num < 10) return num; 
+		string new_str = to_string(num);
+		
+		vector<pair<int, int>> minleft;
+		minleft.push_back(make_pair(new_str[0], 0));
+		for (int i = 1; i < new_str.length(); ++ i) {
+			if (new_str[i] < minleft.back().first)
+				minleft.push_back(make_pair(new_str[i], i));
+			else minleft.push_back(minleft.back());
+		}
+
+		vector<pair<int, int>> maxright;
+		maxright.push_back(make_pair(new_str.back(), new_str.length()-1));
+		for (int i = new_str.length()-2; i >= 0; -- i) {
+			if (new_str[i] > maxright.back().first)
+				maxright.push_back(make_pair(new_str[i], i));
+			else maxright.push_back(maxright.back());
+		}
+
+		for (int i = 0; i < new_str.size(); ++ i) {
+			if (maxright.back().first != minleft[i].first) {
+				new_str[maxright.back().second] = minleft[i].first;
+				new_str[minleft[i].second]  = maxright.back().first;
+				break;
+			}
+            maxright.pop_back();
+		}
+
+		return stoi(new_str);
+	}
+};
+
+/*
+the idea is: use a vector minfromleft to record the minimum number and its index 
+for current pointer location viewing from its left, and a maxfromright to record the maximun number and its
+index for current pointer location viewing from its right;
+then we go through the two vector from left to right, once we find the value is different,
+we have found the swap pair;
+the proof is for i and j position (i < j), i will see more possibilities of max candiate from its right
+while j will see more possibilities of min candiate from its left, however, i is higher digit than j
+that means replacing i will grant us a more siginificant number, so do it
+ */
