b7d612003ccbf5f55bc13657ba240155aaf2c6e7
Liurunex <liurunex@gmail.com>
2017-10-19 15:43:08 -0400

    77 added

---
+++ 77_ Combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>> res;
+		vector<int> comb;
+		dfsbacktracking(res, comb, n, k, 1);
+		return res;
+	}
+
+	void dfsbacktracking(vector<vector<int>> &res, vector<int> &comb, int &n, int k, int start) {
+		if (comb.size() == k) {
+			res.push_back(comb);
+			return;
+		}
+
+		for (int i = start; i <= n; ++ i) {
+			comb.push_back(i);
+			dfsbacktracking(res, comb, n, k, i+1);
+			comb.pop_back();
+		}
+	}
+};
