2f968e2688cbd668adfa061594c98cf00080a549
Liurunex <liurunex@gmail.com>
2017-11-01 16:51:29 -0400

    345 added two pointers + unordered_set

--- 345_Reverse Vowels of a String.cpp
+++ 345_Reverse Vowels of a String.cpp
@@ -1,26 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
-private:
-	int res = 0;
 public:
-	int closestValue(TreeNode* root, double target) {
-		return help(root, target).second;
-	}
-
-	pair<double, int> help(TreeNode* root, double target) {
-		if (!root) return make_pair(-1,0);
-		double dis_cur = abs(root->val - target);
-		TreeNode* checksub = target > root->val ? root->right:root->left;
-		auto sub = help(checksub, target);
-		if (sub.first == -1 || sub.first > dis_cur) return make_pair(dis_cur, root->val);
-		return sub;
+	string reverseVowels(string s) {
+		unordered_set<char> dict ({'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'});
+		int left = 0, right = s.length()-1;
+		while (left < right) {
+			while (!dict.count(s[left])) left ++;
+			while (!dict.count(s[right])) right --;
+            if (left >= right) return s; 
+			swap(s[left ++], s[right --]);
+		}
+		return s;
 	}
 };
+/* the idea: intuitive a tow pointer + unorderset problem;
+ * Notice the vowels contains both upper-lowercase letter
+ */
