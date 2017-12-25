18766fdfddb8f321a2f9c504aec06159dd90ea84
Liurunex <liurunex@gmail.com>
2017-12-25 12:10:05 -0500

    744 added binary search

---
+++ 744_Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	char nextGreatestLetter(vector<char>& letters, char target) {
+		int left = 0, right = letters.size();
+		while (left <= right) {
+			int mid = left + (right - left)/2;
+			if (letters[mid] > target)
+				right = mid-1;
+			else 
+				left = mid+1;
+		}
+		return letters[left] > target ? letters[left]:letters[0];
+	}
+};
