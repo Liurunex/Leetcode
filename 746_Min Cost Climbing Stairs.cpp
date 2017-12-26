f028c488f99fe6bbf3d079443c79325a21c5ded1
Liurunex <liurunex@gmail.com>
2017-12-26 10:47:54 -0800

    746 added, dp notice the dp size should be increase by 1

---
+++ 746_Min Cost Climbing Stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+		if (cost.size() < 3) return 0;
+		vector<int> dp (cost.size()+1, 0);
+		for (int i = 2; i < cost.size()+1; ++ i) {
+			dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]); 
+		}
+		return dp[cost.size()];
+	}
+};
