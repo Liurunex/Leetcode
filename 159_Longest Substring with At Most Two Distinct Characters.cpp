0775d6fc4ceb3c1d0318ed7d89ce4cc1132c3e5c
Liurunex <liurunex@gmail.com>
2018-01-08 14:25:18 -0500

    159+340 added two pointer + hashmap

---
+++ 159_Longest Substring with At Most Two Distinct Characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	int lengthOfLongestSubstringTwoDistinct(string s) {
+		int i = 0, j = 0, res = 0;
+		unordered_map<char, int> dict;
+		while (j < s.size()) {
+			dict[s[j]] ++;
+			while (dict.size() > 2) {
+                if (!(-- dict[s[i]]))
+					dict.erase(s[i]);
+                i ++;
+            }
+			res = max(res, 1-i+(j++));
+		}
+
+		return res;
+	}
+};
+
+/* the idea: slipping window + two pointer + hashmap. We set two
+ * pointers, one for leftbound, one for rightbound; the hashmap
+ * mapped the char to its appearing times in the slipping window.
+ * Keep increase the rightbound pointer and so as the char's values
+ * inside the hash-map. Once we found the hashmap has more than 2
+ * entries, we need to move the leftbound pointer and so as reduce
+ * the char's appearing times in hashmap until the hashmap has no
+ * more than 2 entries.
+ */
